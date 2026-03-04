947cd63c119f8add9971704f844fd406d433a519
ibougajd <ilyassbougajdi1337@gmail.com>
2026-03-04 08:52:06 +0000

    Optimizing Docker image size

--- services/chat/src/app.module.ts
+++ services/chat/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 import { PrismaService } from './prisma.service';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [MessagesModule],
   controllers: [AppController],
-  providers: [PrismaService],
+  providers: [PrismaService, AppService],
 })
 export class AppModule {}
